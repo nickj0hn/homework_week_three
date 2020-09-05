e39be8251c9446c6cb2a3a413145ac812788e13f
Nick John <nick.john@mail.com>
2020-09-05 22:47:45 +0930

    playing around with code to determine password length with user input

--- script.js
+++ script.js
@@ -9,17 +9,16 @@ function writePassword(){
 }
 
 function generatePassword() {
-  // var firstPrompt = (prompt("How long do you want your password to be(must be between 8 and 128"));
+  var passwordLength = (prompt("How long do you want your password to be? (must be between 8 and 128)"));
+
+  // using code from https://stackoverflow.com/questions/17907455/how-to-get-numeric-value-from-a-prompt-box in order to plug it in to code below
+  var userLengthChoice = parseInt(passwordLength);
 
 
   // using code from https://www.codespeedy.com/how-to-generate-a-random-password-in-javascript/ the below code is generating random passwords, I've hard-coded the max and min lengths for the sake of testing. 
   // now I need to reintroduce the prompt commented out above, plus all the other questions, add 'for loops' for each, and split all the character options below into seperate variables and recombine them depending on user selection. 
-  
-  var max = 128
-  var min = 8
   var passwordChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@!%&()/";
-  var randPwLen = Math.floor(Math.random() * (max - min + 1)) + min;
-  var randPassword = Array(randPwLen).fill(passwordChars).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
+  var randPassword = Array(userLengthChoice).fill(passwordChars).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
   return randPassword;
 }
 
