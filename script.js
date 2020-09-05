cef11996f9a721d8f96080beef2677a7bc205a22
Nick John <nick.john@mail.com>
2020-09-05 23:36:07 +0930

    Split passwordChars’ characters into their individual groups Introduced user prompts for criteria, and then used the confirm()method and added the required variables together to create a new passwordChars to be randomised.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate").addEventListener("click",writePassword);
+
+// have separated out character options, lowercase will be the default and remains 'passwordChars' 
+var passwordNum = "0123456789";
+var passwordUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordSpec = "#@!%&()/";
+
+var passwordChars = "abcdefghijklmnopqrstuvwxyz";
 
 
 function writePassword(){
@@ -12,19 +19,33 @@ function generatePassword() {
   var passwordLength = (prompt("How long do you want your password to be? (must be between 8 and 128)"));
 // used while statement to add restrictions on length. (https://www.w3schools.com/JSREF/jsref_while.asp)
   while(passwordLength <= 7 || passwordLength >= 129) {
-
     alert("Password length must be between 8 and 128 Please re-enter preference.");
-
     var passwordLength = (prompt("How long do you want your password to be? (must be between 8 and 128)"));
     } 
-
   // using code from https://stackoverflow.com/questions/17907455/how-to-get-numeric-value-from-a-prompt-box in order to plug it in to code below
   var userLengthChoice = parseInt(passwordLength);
 
+  // using 'confirm() Method' - https://www.w3schools.com/jsref/met_win_confirm.asp 
 
-  // using code from https://www.codespeedy.com/how-to-generate-a-random-password-in-javascript/ the below code is generating random passwords, I've hard-coded the max and min lengths for the sake of testing. 
-  // now I need to reintroduce the prompt commented out above, plus all the other questions, add 'for loops' for each, and split all the character options below into seperate variables and recombine them depending on user selection. 
-  var passwordChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@!%&()/";
+  var upperCase = (confirm("Do you wish to include uppercase characters?"));
+    if (upperCase == true) {
+      var passwordChars = passwordChars + passwordUpper;
+    }
+  
+  var upperCase = (confirm("Do you wish to include special characters?"));
+  if (upperCase == true) {
+   var passwordChars = passwordChars + passwordSpec;
+    }
+
+  var upperCase = (confirm("Do you wish to include numbers?"));
+   if (upperCase == true) {
+     var passwordChars = passwordChars + passwordNum;
+     }
+
+
+
+
+  // using code from https://www.codespeedy.com/how-to-generate-a-random-password-in-javascript/ 
   var randPassword = Array(userLengthChoice).fill(passwordChars).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
   return randPassword;
 }
